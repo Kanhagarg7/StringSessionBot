0d2fd291ee50d2cd7320342c3676a1e7b758a201
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2025-01-07 23:32:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import env
 import config
 import logging
 from pyrogram import Client, idle
-from pyromod import listen
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 
